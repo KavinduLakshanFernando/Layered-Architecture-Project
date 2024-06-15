3faf5b64596ee6e42f91aeb507f6c81333601227
KavinduLakshanFernando <kavindu2003fernando@gmail.com>
2024-06-15 20:23:59 +0530

    Layered Architecture

---
+++ src/main/java/com/example/layeredarchitecture/dao/ItemDao.java
@@ -0,0 +1,21 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+import com.example.layeredarchitecture.model.ItemDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public interface ItemDao {
+    public ArrayList<ItemDTO> loadAllItem() throws SQLException, ClassNotFoundException;
+
+    public void deleteItem(String code) throws SQLException, ClassNotFoundException;
+
+    public void SaveItem(ItemDTO Idto) throws SQLException, ClassNotFoundException;
+
+    public void updateItem(ItemDTO I) throws SQLException, ClassNotFoundException;
+
+    public boolean existItem (String code) throws SQLException, ClassNotFoundException;
+
+    public String newId() throws SQLException, ClassNotFoundException;
+}
